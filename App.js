0d00aa1ba669b29434a190327c6163063303592f
Subhomita2005 <subhomita2019@gmail.com>
2020-08-29 20:54:14 +0530

    added bottomTabnavigator

--- App.js
+++ App.js
@@ -1,15 +1,23 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import BookTransactionScreen from "./screens/BookTransactionScreen"
+import SearchScreen from "./screens/SearchScreen"
+import {createAppContainer} from 'react-navigation'
+import {createBottomTabNavigator} from 'react-navigation-tabs'
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+export default class App extends React.Component{
+  render(){
+    return(
+      <AppContainer></AppContainer>
+    )
+  }
+} 
+const TabNavigator= createBottomTabNavigator({
+  Transaction :{screen:BookTransactionScreen},
+  Search :{screen:SearchScreen}
+})
+const AppContainer = createAppContainer(TabNavigator)
 
 const styles = StyleSheet.create({
   container: {
